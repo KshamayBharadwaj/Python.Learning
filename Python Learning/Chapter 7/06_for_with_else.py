0e2f92de5f123cbf498759b623d1623d4694efeb
Kshamay Bharadwaj <kshamaypbharadwaj@gmail.com>
2024-10-15 09:17:54 +0530

    Add files via upload

---
+++ Python Learning/Chapter 7/06_for_with_else.py
@@ -0,0 +1,6 @@
+l=[1,34,56,77]
+
+for i in l:
+    print(i)
+else:
+    print("Done")
---
+++ Python Learning/Chapter 7/07_break_and_continue.py
@@ -0,0 +1,9 @@
+for i in range(100):
+    if(i==35):
+        break  #Exit the loop right now
+    print(i)
+
+for i in range(100):
+    if(i==35):
+        continue # Skip this iteration
+    print(i)
